628eadad57e39af7118c234b209bdbfd482a5615
Sachin Kumar <sachin80510@gmail.com>
2026-08-25 01:24:12 +0530

    feat: implement nested reviews system with author authorization

--- routes/review.js
+++ routes/review.js
@@ -2,39 +2,35 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
- 
 
 //Reviews
 //Post Review Route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+    console.log(newReview);
+    listing.reviews.push(newReview);
 
-  listing.reviews.push(newReview);
-
-  await newReview.save();
-  await listing.save();
-  req.flash("success", "New Review Created");
-
-  res.redirect(`/listings/${listing._id}`);
-}));
-
+    await newReview.save();
+    await listing.save();
+    req.flash("success", "New Review Created!");
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
 //Delete Review Route
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
 
