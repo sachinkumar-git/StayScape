82e3377228c48f8f5d35b7da4fc61bcc0f85f0b2
Sachin Kumar <sachin80510@gmail.com>
2026-08-29 22:15:47 +0530

    feat: update API routes for modular structure

--- routes/review.js
+++ routes/review.js
@@ -1,44 +1,23 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
-
-
-//Reviews
-//Post Review Route
-router.post(
-  "/",
-  isLoggedIn,
+const {
   validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    console.log(newReview);
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "New Review Created!");
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-//Delete Review Route
-router.delete(
-  "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
+} = require("../middleware.js");
 
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted");
-    res.redirect(`/listings/${id}`);
-  })
-);
+const reviewController = require("../controllers/reviews.js");
+const review = require("../models/review.js");
+
+//Post Route
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+
+//Delete Review Route
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.destroyReview));
+
 
 module.exports = router;
